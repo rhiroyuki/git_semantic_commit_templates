f482f03e8d9955b604ba6e10fbd060caf0a26767
Ricardo Eihara <ricardo.hejr@gmail.com>
2019-05-02 19:26:55 -0300

    feat(CommitTemplates): add templates
    This commit adds all the templates to be utilized whenever you want to
    make a semantic commit.
    
    By issuing the command `$ gc` following with a semantic type it will
    open a commit message using a templated pre-determined on
    `commit_templates` folder.
    
    Ex: `$ gc feature` or `$ gc chore`
    
    A script was created in order to handle and determine which template is
    supposed to be used.

---
+++ git_commit.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+gc() {
+  for file in "$HOME"/.git_commit_templates/commit_templates/*;
+  do
+    if [ "$(basename "$file")" = "$1" ]; then
+      git commit --template="$HOME/.git_commit_templates/commit_templates/$1" "${@:2}"
+      return 0
+    fi
+  done
+
+  git commit "${@:2}"
+}
